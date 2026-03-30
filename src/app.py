80391a3bcf4cf68d81e5e799b4c242a77c14658c
DIVYA YELMAKANNE <divyayelmakanne@gmail.com>
2026-03-31 00:33:18 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -572,7 +572,6 @@ def result():
                              mspnextyear=mspnextyear,
                              minPriceNextYear=avgPriceNextyear
                              )
-
 if __name__=="__main__":
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8000))  # changed 5000 → 8000
     app.run(host='0.0.0.0', port=port)
